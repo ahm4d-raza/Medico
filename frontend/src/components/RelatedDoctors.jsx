2cd3a679234c9ff412265822401854bffe8d3724
Ahmad Raza <ahmad009raza@gmail.com>
2026-07-06 13:46:02 +0530

    Getting error

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate, useParams } from 'react-router-dom'
+import {useContext, useState, useEffect} from 'react'
+
+const RelatedDoctors = ({speciality,docId}) => {
+const { doctors } = useContext(AppContext)
+const navigate = useNavigate()
+
+const [relDocs, setRelDocs] = useState([])
+
+React.useEffect(()=>
+{
+     if (doctors.length > 0 && speciality ) {
+          const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id != docId)
+          setRelDocs(doctorsData)
+     }
+},[doctors,speciality])
+
+  return (
+    <div>
+      <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+      <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+      <p className='sm:w-1/3 text-center text-sm'>simply browse through our extensive list of trusted doctors.</p>
+      <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 lg:gap-6 pt-5 gap-y-6 px-3 sm:px-0'>
+          {relDocs?.slice(0, 5).map((item, index)=>(
+               <div onClick={()=>navigate(`/appoinment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <img className='bg-blue-50' src={item.image} alt="" />
+                    <div className='p-4 bg-white'>
+                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                          <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                         </div>
+                         <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                         <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                    </div>
+               </div>
+          ))}
+      </div>
+      <button onClick={()=>{ navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+    </div>
+    </div>
+  )
+}
+
+export default RelatedDoctors
